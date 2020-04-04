c7f24aa6e8454c2d08edb04f52408f8418d431b1
aitzolsoto <56816883+aitzolsoto@users.noreply.github.com>
2020-04-04 15:02:17 +0200

    Add files via upload

---
+++ Ejercicios/semana2/echo.php
@@ -0,0 +1,20 @@
+<?php
+
+if (isset($_GET["user"]))
+    echo $_GET["user"];
+
+    echo "<br>";
+if (isset($_GET["pswd"]))
+    echo $_GET["pswd"];
+
+
+if (isset($_GET["optradio"]))
+echo $_GET["optradio"];
+
+    echo "<br>";
+if (isset($_GET["email"]))
+echo $_GET["email"];
+    
+    echo "<br>";
+if (isset($_GET["puntos"]))
+echo $_GET["puntos"];
